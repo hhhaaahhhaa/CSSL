290e005cd6f6a4f8531e72ebe776d32769a56d77
hhhaaahhhaa <thomas1232121@gmail.com>
2023-12-12 17:27:48 +0800

    add urbansound8k

--- lightning/downstream/sound_classification/local/preprocessors/__init__.py
+++ lightning/downstream/sound_classification/local/preprocessors/__init__.py
@@ -3,12 +3,14 @@ from typing import Type
 from dlhlp_lib.parsers.Interfaces import BasePreprocessor
 
 from .esc50 import ESC50Preprocessor
+from .urbansound8k import UrbanSound8KPreprocessor
 from .fma import FMAMusicPreprocessor
 from .vocal_set import VocalSetPreprocessor
 
 
 PREPROCESSORS = {
     "ESC50": ESC50Preprocessor,
+    "UrbanSound8K": UrbanSound8KPreprocessor,
     "FMA": FMAMusicPreprocessor,
     "VocalSet": VocalSetPreprocessor,
 }
